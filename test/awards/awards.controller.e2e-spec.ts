66075368f7eda5dc200ec05a4a40760fae9105f7
Tiago Gois <goistsg@gmail.com>
2025-05-14 14:16:05 -0300

    test(check-csv): add check csv file and rename tests with mocked process

--- test/awards/awards.controller.e2e-spec.ts
+++ test/awards/awards.controller.e2e-spec.ts
@@ -1,77 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { getRepositoryToken } from '@nestjs/typeorm';
 import * as request from 'supertest';
 import { AppModule } from '../../src/app.module';
-import { AwardsService } from '../../src/awards/services/awards.service';
-import { AwardsServiceMock } from '../mocks/awards.service.mock';
-import { movieRepositoryMock } from '../mocks/movie.repository.mock';
-import { Movie } from '../../src/awards/entities/movie.entity';
-import { movieMock } from '../mocks/movie.mock';
-import { createMovieDtoMock } from '../mocks/create.movie.dto..mock';
+import { MovielistLoaderService } from '../../src/shared/services/movielist-loader.service';
 
-describe('AwardsController (e2e)', () => {
+describe('AwardsController check csv file (e2e)', () => {
   let app: INestApplication;
-  let createdId: number;
+  let movielistLoaderService: MovielistLoaderService;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })
-      .overrideProvider(AwardsService)
-      .useValue(AwardsServiceMock)
-      .overrideProvider(getRepositoryToken(Movie))
-      .useValue(movieRepositoryMock)
-      .compile();
+    }).compile();
 
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
     await app.init();
+
+    movielistLoaderService = moduleFixture.get(MovielistLoaderService);
   });
 
-  it('POST /awards - deve criar um novo prêmio', async () => {
-    const response = await request(app.getHttpServer())
-      .post('/awards')
-      .send(createMovieDtoMock)
-      .expect(201);
-    
-    expect(response.body).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({ title: 'Filme Mockado' })
-      ])
-    );
-  });
+  beforeEach(async () => {
+    jest.clearAllMocks();
+  })
 
-  it('GET /awards - deve listar prêmios', async () => {
+  it('GET /awards - deve retornar exatamente os dados do arquivo CSV', async () => {
+    const records = await movielistLoaderService.loadMoviesFromCsv();
+
     const response = await request(app.getHttpServer())
-      .get('/awards')
+      .get('/awards?limit=1000')
       .expect(200);
 
-    expect(response.body).toEqual([
-      {
-        id: 1,
-        year: 2022,
-        title: 'Filme Mockado',
-        studios: 'Estúdio Mock',
-        producers: 'Produtor Mock',
-        winner: false,
-      },
-    ]);
-  });
+    const responseWithoutId = response.body.map(({ id, ...rest }) => rest);
 
-  it('PUT /awards - deve atualizar um prêmio', async () => {
-    await request(app.getHttpServer())
-      .put('/awards')
-      .send(movieMock)
-      .expect(200);
-  });
-
-  it('DELETE /awards/:id - deve remover um prêmio', async () => {
-    const id = 1
-    await request(app.getHttpServer())
-      .delete(`/awards/${id}`)
-      .expect(200)
-      .expect({});
+    const sortFn = (a, b) => a.title.localeCompare(b.title);
+    expect(responseWithoutId.sort(sortFn)).toEqual(records.sort(sortFn));
   });
 
   afterAll(async () => {
